a73aed3871111faf247c88520fa21cf45fc81e2d
Pamin Rangsikunpum <pamin2222@hotmail.com>
2016-08-31 16:23:20 +0700

    implement view transfer result page

--- app/src/main/java/com/example/rockroku/somkiatbankmobilebanking/TransferResultActivity.java
+++ app/src/main/java/com/example/rockroku/somkiatbankmobilebanking/TransferResultActivity.java
@@ -2,37 +2,38 @@ package com.example.rockroku.somkiatbankmobilebanking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.rockroku.somkiatbankmobilebanking.common.BaseActivity;
 
-public class ConfirmActivity extends BaseActivity implements View.OnClickListener {
-
-    private Button mBtConfirm;
+public class TransferResultActivity extends BaseActivity implements View.OnClickListener {
+    private Button mBtBackToHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_confirm);
+        setContentView(R.layout.activity_transfer_result);
 
         bindView();
-
         setViewListener();
     }
 
     private void bindView() {
-        mBtConfirm = (Button) findViewById(R.id.btn_confirm);
+        mBtBackToHome = (Button) findViewById(R.id.bt_back_to_home);
     }
 
     private void setViewListener() {
-        mBtConfirm.setOnClickListener(this);
+        mBtBackToHome.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        if (view == mBtConfirm) {
-            openActivity(TransferResultActivity.class);
+        if (view == mBtBackToHome) {
+            Log.v("TransferResultActivity", "onClick mBtBackToHome");
+            openActivity(MainActivity.class);
         }
     }
 }
+
