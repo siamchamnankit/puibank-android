26ec70540ab1016922112fb36f1382d3b6969e04
Pamin Rangsikunpum <pamin2222@hotmail.com>
2016-08-31 16:12:13 +0700

    implement confirm page view

---
+++ app/src/main/java/com/example/rockroku/somkiatbankmobilebanking/ConfirmActivity.java
@@ -0,0 +1,16 @@
+package com.example.rockroku.somkiatbankmobilebanking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class ConfirmActivity extends AppCompatActivity {
+
+    private Button mBtConfirm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm);
+    }
+}
--- app/src/main/java/com/example/rockroku/somkiatbankmobilebanking/TransferActivity.java
+++ app/src/main/java/com/example/rockroku/somkiatbankmobilebanking/TransferActivity.java
@@ -2,12 +2,37 @@ package com.example.rockroku.somkiatbankmobilebanking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class TransferActivity extends AppCompatActivity {
+import com.example.rockroku.somkiatbankmobilebanking.common.BaseActivity;
+
+public class TransferActivity extends BaseActivity implements View.OnClickListener {
+
+    private Button mBtTransfer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transfer);
+
+        bindView();
+        setViewListener();
+    }
+
+    private void bindView() {
+        mBtTransfer = (Button) findViewById(R.id.bt_transfer_transfer_activity);
+    }
+
+    private void setViewListener() {
+        mBtTransfer.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mBtTransfer) {
+            // open confirm page
+            openActivity(ConfirmActivity.class);
+        }
     }
 }
